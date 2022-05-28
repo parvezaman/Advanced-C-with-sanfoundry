342b2e6bc4e35e90ea4c2f95d97c20166a80510e
parvezaman <emailtoamanullah@gmail.com>
2022-05-28 17:41:30 +0600

    solved test 5

--- Tests/test_5.c
+++ Tests/test_5.c
@@ -3,16 +3,18 @@ int main()
 {
     int a = 1, b = 3, c = 2;
     if (a > b)
-    {
+
         if (b > c)
-        {
+
             printf("a>b and b>c\n");
-        }
-    }
-    else
-    {
-        printf("something else");
-    }
+
+        else
+
+            printf("something else");
 
     return 0;
 }
+
+/*
+the else statement is adjusted with second if (b>c) not the first one. thats why it prints nothing
+ */
