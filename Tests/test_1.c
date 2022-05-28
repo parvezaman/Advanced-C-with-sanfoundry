8c3aae4d1f6c998589110fb40e89171dbf6eec67
parvezaman <emailtoamanullah@gmail.com>
2022-05-28 15:08:41 +0600

    test 1 explanation

--- Tests/test_1.c
+++ Tests/test_1.c
@@ -12,3 +12,17 @@ int main()
 
     return 0;
 }
+
+/*
+well, in case of binary operators(operators which take two operands to compare or operate ec: >, < etc) you need both sides to be the same type.
+
+we know there are different types of int as: int, unsigned int, unsigned long etc.
+
+Generally sizeof() function takes unsigned int.
+but in this case this operation is having something like:
+unsigned(int) > signed(-1)
+
+so it is becoming filthy logic and it is returning "False"
+
+PS. Read about various conversion rules in C programmig
+ */
