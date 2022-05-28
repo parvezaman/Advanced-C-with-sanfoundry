667d605ab83cf7b3a588b0475c612296cd4c6907
parvezaman <emailtoamanullah@gmail.com>
2022-05-28 14:40:02 +0600

    test 2 added

---
+++ test_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    float f = 0.1;
+    if (f == 0.1)
+    {
+        printf("True\n");
+    }
+    else
+    {
+        printf("False\n");
+    }
+
+    return 0;
+}
