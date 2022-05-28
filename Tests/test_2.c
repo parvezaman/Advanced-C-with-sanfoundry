a85378310af88c17ddb619102857173dae9f6f4f
parvezaman <emailtoamanullah@gmail.com>
2022-05-28 15:15:06 +0600

    solved second test

--- Tests/test_2.c
+++ Tests/test_2.c
@@ -13,3 +13,12 @@ int main()
 
     return 0;
 }
+
+/*
+ when we put any decimal number like 0.1 directly into  hte code it is treated like double. And double has 10 number of precesion and on the other hand float has 6 number of precesions.
+
+ again the operator should have same type of data in both sides to compare. but in t his case it is getting
+ flot == double (which is incorrect)
+
+thats why the answer is "False"
+ */
