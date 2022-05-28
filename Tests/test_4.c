b02007c5711a336afb341f9cf0f1485918eef9f7
parvezaman <emailtoamanullah@gmail.com>
2022-05-28 17:36:40 +0600

    solved test 4

--- Tests/test_4.c
+++ Tests/test_4.c
@@ -6,3 +6,9 @@ int main()
     printf("p = %c\n", *p);
     return 0;
 }
+// segmentation fault (core dumped)
+
+/*
+By *p = 0 is as defining a location with null address. And then by *p = 'A' is assigning 'A' into that null adress. which is logically incorrect. because how can you put something into a adress which is not avaialable.
+thats why it is giving segmentation faulat. beacause you are trying to access a momory location where you dont have any access.
+ */
